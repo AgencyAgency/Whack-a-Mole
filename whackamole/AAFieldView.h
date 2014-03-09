47f470435a36f9b9de421dd2e843b33f8c911b66
Kyle Oba <koba@mudphone.com>
2014-03-08 19:25:22 -1000

    Add new field view class for half the arena.

---
+++ whackamole/AAFieldView.h
@@ -0,0 +1,13 @@
+//
+//  AAFieldView.h
+//  whackamole
+//
+//  Created by Kyle Oba on 3/8/14.
+//  Copyright (c) 2014 AgencyAgency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAFieldView : UIView
+
+@end
---
+++ whackamole/AAFieldView.m
@@ -0,0 +1,31 @@
+//
+//  AAFieldView.m
+//  whackamole
+//
+//  Created by Kyle Oba on 3/8/14.
+//  Copyright (c) 2014 AgencyAgency. All rights reserved.
+//
+
+#import "AAFieldView.h"
+
+@implementation AAFieldView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
